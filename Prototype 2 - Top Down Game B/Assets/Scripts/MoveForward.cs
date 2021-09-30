47d19b670b7b52b1edf2dc542e607f79ae309a7a
matthewslider <matthew.s424@stu.nebo.edu>
2021-09-30 09:20:31 -0600

    MoveForward script

--- Prototype 2 - Top Down Game B/Assets/Scripts/MoveForward.cs
+++ Prototype 2 - Top Down Game B/Assets/Scripts/MoveForward.cs
@@ -2,17 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class MoveForward : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 25.0f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
     }
 }
