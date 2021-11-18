cd97eee0e68d6f26fe80373ae9dc426c83cb0405
matthewslider <matthew.s424@stu.nebo.edu>
2021-11-18 09:22:15 -0700

    Added bobbing to pickup and particles

--- Prototype 3 - First Person Game B/Assets/Scripts/Pickup.cs
+++ Prototype 3 - First Person Game B/Assets/Scripts/Pickup.cs
@@ -12,19 +12,26 @@ public class Pickup : MonoBehaviour
 {
     public PickupType type;
     public int value;
+
+    [Header ("Bobbing Anim")]
+
+    public float rotationSpeed;
+
+    public float bobSpeed;
+    public float bobHeight;
+
+    private Vector3 startPos;
+    private bool bobbingUp;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Set the start position
+        startPos = transform.position;
     }
 
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  
     void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
@@ -42,5 +49,18 @@ public class Pickup : MonoBehaviour
             }
             Destroy(gameObject);
         }
+    }
+
+    void Update()
+    {
+        //Rotating
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+
+        //Bob Up and Down
+        Vector3 offset = (bobbingUp == true ? new Vector3(0,bobHeight / 2, 0) : new Vector3(0, -bobHeight /2, 0));
+        transform.position = Vector3.MoveTowards(transform.position, startPos + offset, bobSpeed * Time.deltaTime);
+
+        if(transform.position == startPos + offset)
+            bobbingUp = !bobbingUp;
     }
 }
